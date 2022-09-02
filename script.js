c763d7f27ead096640af2afdae14ba1da6ca8621
swappnet <111042761+swappnet@users.noreply.github.com>
2022-09-02 19:20:45 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 "use strict";
+
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+const mediaQuery = window.matchMedia("(min-width: 768px)");
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 350 ||
+    document.documentElement.scrollTop > 350
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
